03e0e579dae5924305fdbc4e419a813d72fea830
Mélissa Armand <51266327+Sa8th@users.noreply.github.com>
2023-04-18 09:29:39 +0200

    Add files via upload

---
+++ content/RoboflowerSystem/Versions/RoboflowerFiles_2022.10.16/Flower/routines.py
@@ -0,0 +1,173 @@
+import time
+import cv2
+
+filename = 'savedImage.jpg'
+def experiment(mqtt, led, cam, markers, feeder):
+    mqtt.send('debug', 'I woke up')
+
+    led.blink(mqtt.colors['green'])
+
+    mqtt.send('commands', 'awaits')
+
+    while not (mqtt.gotcontents and mqtt.gotrewards):
+        pass
+
+    led.on(mqtt.colors[mqtt.color])
+    print(mqtt.color)
+
+    cam.start()
+    time.sleep(2)
+    mqtt.send('debug', 'camera started')
+
+    mqtt.send('debug', 'Save this time as ')
+    mqtt.send('log', 'NA, NA, NA, NA, NA, Started')
+
+    thisbee = {'id': None, 'pump': None, 'amount': None}
+    feeder.do_clean()  # do a cleaning routine (pump 3 = +12uL, pump 4= -21uL, pump 3 = +9uL)
+
+    while True:
+        now = time.time()
+        frame = cam.grab()
+        markers.detect(frame)
+        # cv2.imshow('f', frame)
+        # cv2.waitKey(1)
+        if markers.bee_arrived:
+            mqtt.send('log', str(markers.last_seen)+', NA, NA, NA, NA, arrived')
+            markers.bee_arrived = False
+        if markers.bee_gone:
+            mqtt.send('log', str(markers.last_seen)+', NA, NA, NA, NA, gone')
+            markers.bee_gone = False
+        if markers.being_seen and markers.IDs is not None and not markers.beenRecognized:
+            thisbeepattern = markers.recognize(mqtt.rewardpatterns)
+            if not markers.beenRecognized:
+                if str(markers.IDs[0][0]) not in (x[1] for x in markers.beenNotified):
+                    mqtt.send('debug', 'Seen a  marker not expected - ' + str(markers.IDs[0][0]))
+                    markers.beenNotified.append((now, str(markers.IDs[0][0])))
+                for t in list(markers.beenNotified):
+                    if now-t[0] > 30:
+                        markers.beenNotified.remove(t)                            
+                
+            elif markers.beenRecognized:
+                ID = markers.IDs[0][0]
+                mqtt.send('debug', 'Seen a  marker - ' + str(ID))
+                lastentered = ID
+                cv2.imwrite(filename, frame)
+
+                if not feeder.full:
+                    if ID in feeder.visitors:
+                        if time.time()-feeder.visitors[ID]['when_emptied'] > mqtt.rewardpatterns[thisbeepattern]['retention']:
+                            thisbee['id'] = ID
+                            thisbee['pump'] = mqtt.rewardpatterns[thisbeepattern]['pump']
+                            thisbee['amount'] = mqtt.rewardpatterns[thisbeepattern]['amount']
+                            thisbee['quality'] = mqtt.rewardpatterns[thisbeepattern]['quality']
+                            thisbee['scent'] = mqtt.rewardpatterns[thisbeepattern]['scent']
+
+                            feeder.do_fill(thisbee['pump'], amount=thisbee['amount'])
+                            mqtt.send('debug', 'giving '+str(thisbee['id']) +' sugar from pump '+ str(thisbee['pump']) +
+                                      ' of quality '+ str(thisbee['quality']) +'and scent '+ str(thisbee['scent']) +
+                                      ' for '+ str(thisbee['amount']/1000)+'ul')
+                            mqtt.send('log', str(thisbee['id']) + ','+str(thisbee['pump'])+','+str(thisbee['quality']) +
+                                      ','+str(thisbee['scent'])+','+str(thisbee['amount'])+',filling')
+                            feeder.visitors[ID]['when_filled'] = time.time()
+                        else:
+                            mqtt.send('debug', 'i have seen '+str(ID)+' but the retention time has not elapsed')
+                            mqtt.send('log', str(ID)+', NA, NA, NA, NA, retention')
+                    else:
+                        thisbee['id'] = ID
+                        thisbee['pump'] = mqtt.rewardpatterns[thisbeepattern]['pump']
+                        thisbee['amount'] = mqtt.rewardpatterns[thisbeepattern]['amount']
+                        thisbee['quality'] = mqtt.rewardpatterns[thisbeepattern]['quality']
+                        thisbee['scent'] = mqtt.rewardpatterns[thisbeepattern]['scent']
+
+                        feeder.do_fill(thisbee['pump'], amount=thisbee['amount'])
+                        mqtt.send('debug', 'giving ' + str(thisbee['id']) + ' sugar from pump ' + str(
+                            thisbee['pump']) + ' of quality ' + str(thisbee['quality']) + 'and scent ' + str(
+                            thisbee['scent']) + ' for ' + str(thisbee['amount'] / 1000) + 'ul')
+                        mqtt.send('log', str(thisbee['id']) + ',' + str(thisbee['pump']) + ',' + str(
+                            thisbee['quality']) + ',' + str(thisbee['scent']) + ',' + str(
+                            thisbee['amount']) + ',filling')
+                        feeder.visitors[ID] = {'when_filled': time.time(), 'when_emptied': None}
+                elif feeder.full:
+                    mqtt.send('debug', 'the feeder is full already')
+                    mqtt.send('log', str(ID)+',NA,NA,NA,NA,full')
+        elif not markers.being_seen:
+            if feeder.full:
+                if time.time()-feeder.visitors[thisbee['id']]['when_filled'] > 60:
+                    mqtt.send('debug', 'no bee, emptying the feeder')
+                    mqtt.send('log', 'NA,NA,NA,NA,NA,empty')
+                    feeder.do_flush(80000)  # pump 4 flushes 80uL (wow it's a lot)
+                    feeder.visitors[thisbee['id']]['when_emptied']=time.time()
+        if mqtt.got_prime_message:
+            feeder.do_prime()
+            mqtt.got_prime_message=False
+
+        if mqtt.got_kill_message:
+            mqtt.send('debug', 'I got turned off :( Goodnight!')
+            mqtt.send('log', 'NA,NA,NA,NA,NA,Quit')
+            led.off()
+            break
+
+
+def colonylearn(mqtt, led, cam, markers, feeder):
+    seenmarkers = []
+    mqtt.send('debug', 'I woke up in learn mode')
+
+    led.off()
+
+    mqtt.send('commands', 'awaits')
+
+    cam.start()
+    time.sleep(2)
+    mqtt.send('debug', 'camera started')
+
+    mqtt.send('debug', 'Save this time as 0')
+    mqtt.send('log', 'NA, NA, NA, NA, NA, LearnStarted')
+
+    thisbee = {'id': None, 'pump': None, 'amount': None}
+    while True:
+        frame = cam.grab()
+        markers.detect(frame)
+        cv2.imshow('f', frame)
+        cv2.waitKey(1)
+        if markers.being_seen and markers.IDs is not None and not markers.beenRecognized:
+            markers.beenRecognized = True
+            ID = markers.IDs[0][0]
+            seenmarkers.append(ID)
+            mqtt.send('debug', 'Seen a  marker - ' + str(ID))
+
+            if not feeder.full:
+                if time.time()-feeder.when_emptied > 60:
+                    thisbee['id'] = ID
+                    thisbee['pump'] = 0
+                    thisbee['amount'] = 50000
+                    thisbee['quality'] = 'null'
+                    thisbee['scent'] = 'null'
+
+                    print(thisbee['amount'])
+                    feeder.do_fill(thisbee['pump'], amount=thisbee['amount'])
+                    mqtt.send('debug', 'giving '+str(thisbee['id'])+' sugar from pump '+str(thisbee['pump']) +
+                              ' of quality '+str(thisbee['quality'])+'and scent '+str(thisbee['scent']) +
+                              ' for '+str(thisbee['amount']/1000)+'ul')
+                    mqtt.send('log', str(thisbee['id'])+','+str(thisbee['pump']) +
+                              ','+str(thisbee['quality'])+','+str(thisbee['scent']) +
+                              ','+str(thisbee['amount'])+',filling')
+                else:
+                    mqtt.send('debug', 'i have seen '+str(ID)+' but the retention time has not elapsed')
+                    mqtt.send('log', str(ID)+',NA,NA,NA,NA,retention')
+            elif feeder.full:
+                mqtt.send('debug', 'the feeder is full already')
+
+        elif not markers.being_seen:
+            if feeder.full:
+                mqtt.send('debug', 'no bee, emptying the feeder')
+                feeder.do_flush(100000)
+        if mqtt.got_prime_message:
+            feeder.do_prime()
+            mqtt.got_prime_message = False
+
+        if mqtt.got_kill_message:
+            mqtt.send('debug', 'I got turned off :( Goodnight!')
+            mqtt.send('debug', 'in the end I have seen today: '+str(seenmarkers))
+            mqtt.send('log', 'NA,NA,NA,NA,NA,Quit')
+            led.off()
+            break
---
+++ content/RoboflowerSystem/Versions/RoboflowerFiles_2022.10.16/Hub/connect.py
@@ -0,0 +1,152 @@
+from paho.mqtt.client import Client
+import time  # module to handle time-related tasks
+import yaml  # human-friendly file format to read data easily
+from threading import Thread, Event
+
+
+#  This class provides all the necessary functions to connect to an MQTT broker + publish, subscribe, receive, etc.
+class MQTT:
+    def __init__(self, expfolder):
+
+        with open(expfolder+'/contents.yaml', 'r') as file:
+            self.contents = yaml.load(file, Loader=yaml.FullLoader)
+        with open(expfolder+'/flowerGroups.yaml', 'r') as file:
+            self.flowerGroups = yaml.load(file, Loader=yaml.FullLoader)
+        with open(expfolder+'/rewardPatterns.yaml', 'r') as file:
+            self.rewardPatterns = yaml.load(file, Loader=yaml.FullLoader)
+
+        self.myname = 'HUB'
+        self.port = 1883
+
+        self.client = Client(client_id=self.myname)
+
+        self.client.on_connect = self.on_connect  # attach function to callback
+        self.client.on_message = self.on_message
+
+        self.Connected = False
+        self.client.connect('192.168.0.2')
+
+        self.client.loop_start()  # start the loop
+
+        while self.Connected != True:  # Wait for connection
+            time.sleep(0.1)
+
+        self.client.subscribe('commands')
+        self.client.subscribe('log')
+        self.client.subscribe('debug')
+        self.client.subscribe('contents')
+        self.client.subscribe('rewards')
+
+        self.logger = Logger()
+
+        self.ONflowers = []
+        self.stillONflowers = []
+        self.lastcheck = time.time()
+
+    def on_connect(self, client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to broker")
+            self.Connected = True  # Use global variable
+        else:
+            print("Connection failed")
+
+    def on_message(self, client, userdata, message):
+        load = str(message.payload.decode("utf-8"))
+        topic = str(message.topic)
+        timestamp = str(time.ctime())
+        sender = load.split('.')[1]
+        receiver = load.split('.')[2]
+        mess = load.split('.')[3]
+        self.decode(topic, timestamp, sender, mess)
+
+    def decode(self, topic, timestamp, sender, mess):
+        if topic == 'log' or topic == 'debug':
+            self.logger.log(topic, timestamp, sender, mess)
+        elif topic == 'commands':
+            if mess == 'turnedon':
+                self.ONflowers.append(sender)
+            if mess == 'turnedoff':
+                self.ONflowers.remove(sender)
+            if mess == 'stillhere':
+                self.stillONflowers.append(sender)
+                print(sender,' - stillhere')
+            if mess == 'awaits':
+                receiver = sender
+                if receiver in self.flowerGroups['A']:
+                    yourgroup = 'A'
+                if receiver in self.flowerGroups['B']:
+                    yourgroup = 'B'
+                if receiver in self.flowerGroups['C']:
+                    yourgroup = 'C'
+                if receiver in self.flowerGroups['D']:
+                    yourgroup = 'D'
+                mess = str(self.contents[yourgroup][1]['quality']) + ',' + \
+                       str(self.contents[yourgroup][2]['quality']) + ',' + \
+                       str(self.contents[yourgroup][1]['scent']) + ',' +\
+                       str(self.contents[yourgroup][2]['scent']) + ',' +\
+                    str(self.contents[yourgroup]['color'])
+                self.send('contents', mess, receiver)
+                for rewardPattern in self.rewardPatterns:
+                    bees = ''
+                    for bee in self.rewardPatterns[rewardPattern]['bees']:
+                        bees = bees+str(bee)+'-'
+                    bees = bees[:-1]
+                    mess = str(rewardPattern)+','+ bees + ',' + \
+                           str(self.rewardPatterns[rewardPattern][yourgroup]['quality'])+','+ \
+                           str(self.rewardPatterns[rewardPattern][yourgroup]['quantity']) + ',' + \
+                           str(self.rewardPatterns[rewardPattern][yourgroup]['scent']) + ',' + \
+                           str(self.rewardPatterns[rewardPattern][yourgroup]['retention'])
+                    self.send('rewards', mess, receiver)
+
+    def send(self, topic, message, receiver='all'):
+        load = str(time.ctime())+'.'+self.myname+'.'+receiver+'.'+message
+        self.client.publish(topic=topic, payload=load)
+        print(load)
+
+    def keepchecking(self):
+        while True:
+            if len(self.ONflowers) > 0:
+                if time.time()-self.lastcheck > 60:  # checks every minute if flower is still up!
+                    self.send('commands', 'check')
+                    time.sleep(10)
+                    notanswered = list(set(self.ONflowers).difference(set(self.stillONflowers)))
+                    if len(notanswered) > 0:
+                        for dead in notanswered:
+                            self.logger.log('debug', time.ctime(), dead, 'flower '+dead+' is not responding anymore')
+                    self.stillONflowers = []
+                    self.lastcheck = time.time()
+            if self._killer == True:
+                break
+
+    def do_keepchecking(self):
+        self._killer = False
+        self.o = Thread(target=self.keepchecking)
+        self.o.daemon = True
+        self.o.start()
+
+    def stop(self):
+        self._killer = True
+        self.o.join()
+
+class Logger:
+    def __init__(self):
+        self.logname = 'data/'+str(time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime()))+'.log'
+        self.debugname = 'data/'+str(time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime()))+'.debug'
+
+        with open(self.logname, 'w') as f:
+            f.write('time,flower,id,pump,quality,scent,amount,event\n')
+
+        with open(self.debugname, 'w') as f:
+            f.write('time,flower,message\n')
+
+    def log(self, topic, t, s, m):
+        if topic == 'log':
+            ID, pump, quality, scent, amount, event = m.split(',')
+            with open(self.logname, 'a') as f:
+                f.write(t+','+s+','+ID+','+pump+','+quality+','+scent+','+amount+','+event+'\n')
+                print(t+','+s+','+ID+','+pump+','+quality+','+scent+','+amount+','+event)
+        elif topic == 'debug':
+            with open(self.debugname, 'a') as f:
+                f.write(t + ',' + s + ',' + m + '\n')
+                print(t + ',' + s + ',' + m)
+
